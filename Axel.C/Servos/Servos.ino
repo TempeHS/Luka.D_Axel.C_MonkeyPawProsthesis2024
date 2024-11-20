099d228f3636105541202a13b4436b65e366c24f
Axel C <159206642+TheSpiciestRamen@users.noreply.github.com>
2024-11-21 10:32:03 +1100

    Added servo pins

---
+++ Axel.C/Servos/Servos.ino
@@ -0,0 +1,18 @@
+#include <servo.h>
+
+Servo servo1;
+static unsigned int servo1pin = 2;
+
+Servo servo2;
+static unsigned int servo1pin = 3;
+
+Servo servo3;
+static unsigned int servo1pin = 4;
+
+Servo servo4;
+static unsigned int servo1pin = 5;
+
+Servo servo5;
+static unsigned int servo1pin = 6;
+
+
--- MPP/MPP.ino
+++
@@ -1,8 +0,0 @@
-Servo 1 Dgetass
-Servo 2 D1212
-Servo 3 D432
-Servo 4 D27
-Servo 5 D9
-
-Potentiometer Atoobad
-Switch Asignoutnexttime
